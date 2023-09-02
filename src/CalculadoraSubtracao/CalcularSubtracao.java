75f72c41f5d16df8359f52bc178fb83ec25e18a4
Gabriel <143127195+GabrielVFC@users.noreply.github.com>
2023-09-01 21:02:31 -0300

    Update CalcularSubtracao.java

--- src/CalculadoraSubtracao/CalcularSubtracao.java
+++ src/CalculadoraSubtracao/CalcularSubtracao.java
@@ -10,7 +10,7 @@ public class CalcularSubtracao {
         for (int i = 0; i < 5; i++) {
 
             System.out.println("Digite um número:");
-            variaveis.numero = dado.nextInt();
+            variaveis.total(dado.nextInt());
         }
 
         System.out.println("A subtraçao de todos os número é: %d".formatted(variaveis.totalSubtracao()));
